fdeb4ab99b4849ef6f93596fc9f70eb6ae790eaf
shinan <shinan@2dfire.com>
2019-01-09 14:39:58 +0800

    build projetc

---
+++ activity-platform-provider/src/main/java/com/caisl/ActivityPlatformApplication.java
@@ -0,0 +1,14 @@
+package com.caisl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityPlatformApplication.class, args);
+	}
+
+}
+
