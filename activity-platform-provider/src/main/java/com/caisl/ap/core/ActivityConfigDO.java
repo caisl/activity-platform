92229ea1c3eab09ce788574914e5399a61892ff6
caisl <18868831937@163.com>
2019-01-12 18:24:17 +0800

    test git mail change

--- activity-platform-provider/src/main/java/com/caisl/ap/core/ActivityConfigDO.java
+++ activity-platform-provider/src/main/java/com/caisl/ap/core/ActivityConfigDO.java
@@ -1,10 +1,12 @@
 package com.caisl.ap.core;
 
+import java.io.Serializable;
+
 /**
  * ActivityConfigDO
  *
  * @author shinan
  * @since 2019-01-12
  */
-public class ActivityConfigDO {
+public class ActivityConfigDO implements Serializable{
 }
